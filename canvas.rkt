d6ad1720bccf33f71f63f3c7eb7b2e9c722b4311
Calvin Xu <calvinxu806@gmail.com>
2022-08-04 02:04:59 -0700

    Added saving canvas to ppm format string & write to file

--- canvas.rkt
+++ canvas.rkt
@@ -25,6 +25,18 @@
 (define (color-a color)
   (tuple-w color))
 
+(: color->string (->* (Color) (Exact-Nonnegative-Integer) String))
+(define (color->string color [max_color_val 255])
+  (: scale (-> Real Integer))
+  (define (scale frac)
+    (exact-round (* (max 0 (min 1.0 frac)) max_color_val)))
+  (string-append (number->string (scale (color-r color)))
+                 " "
+                 (number->string (scale (color-g color)))
+                 " "
+                 (number->string (scale (color-b color)))
+                 " "))
+
 (: color-op (-> (-> Real Real * Real) Color Color Color))
 (define (color-op op c1 c2)
   (color (op (color-r c1) (color-r c2))
@@ -42,5 +54,66 @@
 (: color* (-> Color (U Color Real) Color))
 (define (color* c arg)
   (color-op * c (if (color? arg) arg (color arg arg arg))))
+
+(struct _canvas
+  ([width : Exact-Positive-Integer]
+   [height : Exact-Positive-Integer]
+   [pixels : (Mutable-Vectorof Color)]) #:prefab #:type-name Canvas)
+
+(: canvas-width (-> Canvas Exact-Positive-Integer))
+(define (canvas-width canvas)
+  (_canvas-width canvas))
+
+(: canvas-height (-> Canvas Exact-Positive-Integer))
+(define (canvas-height canvas)
+  (_canvas-height canvas))
+
+(: canvas-pixels (-> Canvas (Mutable-Vectorof Color)))
+(define (canvas-pixels canvas)
+  (_canvas-pixels canvas))
+
+(: canvas (-> Exact-Positive-Integer Exact-Positive-Integer Canvas))
+(define (canvas width height)
+  (_canvas width height (make-vector (* width height) (color 0 0 0))))
+
+(: pixel-at (-> Canvas Exact-Nonnegative-Integer Exact-Nonnegative-Integer Color))
+(define (pixel-at canvas x y)
+  (if (or (>= x (canvas-width canvas)) (>= y (canvas-height canvas)))
+      (error "Illegal operation: attempting to access pixel out of bounds" x y)
+      (vector-ref (canvas-pixels canvas) (+ (* y (canvas-width canvas)) x))))
+
+(: set-pixel! (-> Canvas Exact-Nonnegative-Integer Exact-Nonnegative-Integer Color Void))
+(define (set-pixel! canvas x y color)
+  (if (or (>= x (canvas-width canvas)) (>= y (canvas-height canvas)))
+      (error "Illegal operation: attempting to set pixel out of bounds" x y)
+      (vector-set! (canvas-pixels canvas) (+ (* y (canvas-width canvas)) x) color)))
+
+(: serialize-canvas (-> Canvas String))
+(define (serialize-canvas canvas)
+  (define header
+    (string-append "P3\n"
+                   (number->string (canvas-width canvas))
+                   " "
+                   (number->string (canvas-height canvas))
+                   "\n"
+                   "255\n"))
+  (define bitmap (vector-map color->string (canvas-pixels canvas)))
+  ;; color->string always adds whitespace at end
+  ;; replace appropriate whitespaces with newlines
+  (begin
+    (let ([PIXELS_PER_ROW 6] [n (* (canvas-width canvas) (canvas-height canvas))])
+      (for ([i (in-range n)])
+        (when (or (= (add1 i) n) (= 0 (remainder (add1 i) PIXELS_PER_ROW)))
+          (let ([curr (vector-ref bitmap i)])
+            (vector-set! bitmap
+                         i
+                         (string-append (substring curr 0 (sub1 (string-length curr))) "\n"))))))
+    (string-append header (string-append* (vector->list bitmap)))))
+
+(: save-canvas (-> Canvas String Void))
+(define (save-canvas canvas filename)
+  (let ([out (open-output-file filename #:mode 'text #:exists 'replace)])
+    (display (serialize-canvas canvas) out)
+    (close-output-port out)))
 
 (provide (except-out (all-defined-out) color-op))
--- tests/canvas-test.rkt
+++ tests/canvas-test.rkt
@@ -12,25 +12,45 @@
     (printf "Failure: tuples not equal ~v, ~v\n" t1 t2)))
 
 (define canvas-test
-  (test-suite "Drawing on a Canvas"
-              (test-case "define color"
-                         (define c (color -0.5 0.4 1.7))
-                         (check-equal? (color-r c) -0.5)
-                         (check-equal? (color-g c) 0.4)
-                         (check-equal? (color-b c) 1.7))
-              (test-case "add color"
-                         (define c1 (color 0.9 0.6 0.75))
-                         (define c2 (color 0.7 0.1 0.25))
-                         (check-tuple= (color+ c1 c2) (color 1.6 0.7 1.0)))
-              (test-case "subtract color"
-                         (define c1 (color 0.9 0.6 0.75))
-                         (define c2 (color 0.7 0.1 0.25))
-                         (check-tuple= (color- c1 c2) (color 0.2 0.5 0.5)))
-              (test-case "multiply color"
-                         (define c (color 0.2 0.3 0.4))
-                         (check-tuple= (color* c 2) (color 0.4 0.6 0.8))
-                         (define c1 (color 1 0.2 0.4))
-                         (define c2 (color 0.9 1 0.1))
-                         (check-tuple= (color* c1 c2) (color 0.9 0.2 0.04)))))
+  (test-suite
+   "Drawing on a Canvas"
+   (test-case "define color"
+              (define c (color -0.5 0.4 1.7))
+              (check-equal? (color-r c) -0.5)
+              (check-equal? (color-g c) 0.4)
+              (check-equal? (color-b c) 1.7))
+   (test-case "add color"
+              (define c1 (color 0.9 0.6 0.75))
+              (define c2 (color 0.7 0.1 0.25))
+              (check-tuple= (color+ c1 c2) (color 1.6 0.7 1.0)))
+   (test-case "subtract color"
+              (define c1 (color 0.9 0.6 0.75))
+              (define c2 (color 0.7 0.1 0.25))
+              (check-tuple= (color- c1 c2) (color 0.2 0.5 0.5)))
+   (test-case "multiply color"
+              (define c (color 0.2 0.3 0.4))
+              (check-tuple= (color* c 2) (color 0.4 0.6 0.8))
+              (define c1 (color 1 0.2 0.4))
+              (define c2 (color 0.9 1 0.1))
+              (check-tuple= (color* c1 c2) (color 0.9 0.2 0.04)))
+   (test-case "color to string"
+              (define c (color 0.2 0.3 0.4))
+              (check-equal? (color->string c) "51 76 102 "))
+   (test-case "create and access canvas"
+              (define c (canvas 10 20))
+              (check-equal? (canvas-width c) 10)
+              (check-equal? (canvas-height c) 20)
+              (define red (color 1 0 0))
+              (set-pixel! c 2 3 red)
+              (check-equal? (pixel-at c 2 3) red))
+   (test-case
+    "save canvas to ppm file"
+    (define c (canvas 5 3))
+    (set-pixel! c 0 0 (color 1.5 0 0))
+    (set-pixel! c 2 1 (color 0 0.5 0))
+    (set-pixel! c 4 2 (color -0.5 0 1))
+    (check-equal?
+     (serialize-canvas c)
+     "P3\n5 3\n255\n255 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n0 0 0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0 255\n"))))
 
 (run-tests canvas-test)
