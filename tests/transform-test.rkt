55dca7f90fee87c6d4732b69fde97bfbf90818ea
Calvin Xu <calvinxu806@gmail.com>
2022-08-09 15:04:40 -0700

    Added scaling & test cases

--- tests/transform-test.rkt
+++ tests/transform-test.rkt
@@ -13,19 +13,38 @@
     (printf "Failure: tuples not equal ~v, ~v\n" t1 t2)))
 
 (define transform-test
-  (test-suite "Translation"
-              (test-case "Multiplying by a translation matrix"
-                         (define t (translation 5. -3. 2.))
-                         (define p (pt -3. 4. 5.))
-                         (check-tuple= (mat-t* t p) (pt 2. 1. 7.)))
-              (test-case "Multiplying by the inverse of a translation matrix"
-                         (define t (translation 5. -3. 2.))
-                         (define inv (inverse t))
-                         (define p (pt -3. 4. 5.))
-                         (check-tuple= (mat-t* inv p) (pt -8. 7. 3.)))
-              (test-case "Translation does not affect vectors"
-                         (define t (translation 5. -3. 2.))
-                         (define v (vec -3. 4. 5.))
-                         (check-tuple= (mat-t* t v) v))))
+  (test-suite "Transformations"
+              (test-suite "Translation"
+                          (test-case "Multiplying by a translation matrix"
+                                     (define t (translate 5. -3. 2.))
+                                     (define p (pt -3. 4. 5.))
+                                     (check-tuple= (mat-t* t p) (pt 2. 1. 7.)))
+                          (test-case "Multiplying by the inverse of a translate matrix"
+                                     (define t (translate 5. -3. 2.))
+                                     (define inv (inverse t))
+                                     (define p (pt -3. 4. 5.))
+                                     (check-tuple= (mat-t* inv p) (pt -8. 7. 3.)))
+                          (test-case "Translation does not affect vectors"
+                                     (define t (translate 5. -3. 2.))
+                                     (define v (vec -3. 4. 5.))
+                                     (check-tuple= (mat-t* t v) v)))
+              (test-suite "scaling"
+                          (test-case "A scaling matrix applied to a point"
+                                     (define t (scale 2. 3. 4.))
+                                     (define p (pt -4. 6. 8.))
+                                     (check-tuple= (mat-t* t p) (pt -8. 18. 32.)))
+                          (test-case "A scaling matrix applied to a vector"
+                                     (define t (scale 2. 3. 4.))
+                                     (define v (vec -4. 6. 8.))
+                                     (check-tuple= (mat-t* t v) (vec -8. 18. 32.)))
+                          (test-case "Multiplying by the inverse of a scaling matrix"
+                                     (define t (scale 2. 3. 4.))
+                                     (define inv (inverse t))
+                                     (define v (vec -4. 6. 8.))
+                                     (check-tuple= (mat-t* inv v) (vec -2. 2. 2.)))
+                          (test-case "Reflection is scaling by a negative value"
+                                     (define t (scale -1. 1. 1.))
+                                     (define p (pt 2. 3. 4.))
+                                     (check-tuple= (mat-t* t p) (pt -2. 3. 4.))))))
 
 (run-tests transform-test)
--- transform.rkt
+++ transform.rkt
@@ -3,8 +3,8 @@
 (require "tuples.rkt")
 (require "matrix.rkt")
 
-(: translation (-> Float Float Float Matrix))
-(define (translation x y z)
+(: translate (-> Float Float Float Matrix))
+(define (translate x y z)
   (build-matrix 4
                 4
                 (lambda ([row : Exact-Nonnegative-Integer] [col : Exact-Nonnegative-Integer])
@@ -13,4 +13,15 @@
                     [(and (= row 0) (= col 3)) x]
                     [(and (= row 1) (= col 3)) y]
                     [(and (= row 2) (= col 3)) z]
+                    [else 0.]))))
+(: scale (-> Float Float Float Matrix))
+(define (scale x y z)
+  (build-matrix 4
+                4
+                (lambda ([row : Exact-Nonnegative-Integer] [col : Exact-Nonnegative-Integer])
+                  (cond
+                    [(and (= row 0) (= col 0)) x]
+                    [(and (= row 1) (= col 1)) y]
+                    [(and (= row 2) (= col 2)) z]
+                    [(and (= row 3) (= col 3)) 1.]
                     [else 0.]))))
