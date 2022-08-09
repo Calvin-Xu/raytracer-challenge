9138d9d4ab640bcc602b060242e4fdd02729ed70
Calvin Xu <calvinxu806@gmail.com>
2022-08-09 15:44:48 -0700

    Added rotation & test cases

--- tests/transform-test.rkt
+++ tests/transform-test.rkt
@@ -13,38 +13,68 @@
     (printf "Failure: tuples not equal ~v, ~v\n" t1 t2)))
 
 (define transform-test
-  (test-suite "Transformations"
-              (test-suite "Translation"
-                          (test-case "Multiplying by a translation matrix"
-                                     (define t (translate 5. -3. 2.))
-                                     (define p (pt -3. 4. 5.))
-                                     (check-tuple= (mat-t* t p) (pt 2. 1. 7.)))
-                          (test-case "Multiplying by the inverse of a translate matrix"
-                                     (define t (translate 5. -3. 2.))
-                                     (define inv (inverse t))
-                                     (define p (pt -3. 4. 5.))
-                                     (check-tuple= (mat-t* inv p) (pt -8. 7. 3.)))
-                          (test-case "Translation does not affect vectors"
-                                     (define t (translate 5. -3. 2.))
-                                     (define v (vec -3. 4. 5.))
-                                     (check-tuple= (mat-t* t v) v)))
-              (test-suite "scaling"
-                          (test-case "A scaling matrix applied to a point"
-                                     (define t (scale 2. 3. 4.))
-                                     (define p (pt -4. 6. 8.))
-                                     (check-tuple= (mat-t* t p) (pt -8. 18. 32.)))
-                          (test-case "A scaling matrix applied to a vector"
-                                     (define t (scale 2. 3. 4.))
-                                     (define v (vec -4. 6. 8.))
-                                     (check-tuple= (mat-t* t v) (vec -8. 18. 32.)))
-                          (test-case "Multiplying by the inverse of a scaling matrix"
-                                     (define t (scale 2. 3. 4.))
-                                     (define inv (inverse t))
-                                     (define v (vec -4. 6. 8.))
-                                     (check-tuple= (mat-t* inv v) (vec -2. 2. 2.)))
-                          (test-case "Reflection is scaling by a negative value"
-                                     (define t (scale -1. 1. 1.))
-                                     (define p (pt 2. 3. 4.))
-                                     (check-tuple= (mat-t* t p) (pt -2. 3. 4.))))))
+  (test-suite
+   "Transformations"
+   (test-suite "Translation"
+               (test-case "Multiplying by a translation matrix"
+                          (define t (translate 5. -3. 2.))
+                          (define p (pt -3. 4. 5.))
+                          (check-tuple= (mat-t* t p) (pt 2. 1. 7.)))
+               (test-case "Multiplying by the inverse of a translate matrix"
+                          (define t (translate 5. -3. 2.))
+                          (define inv (inverse t))
+                          (define p (pt -3. 4. 5.))
+                          (check-tuple= (mat-t* inv p) (pt -8. 7. 3.)))
+               (test-case "Translation does not affect vectors"
+                          (define t (translate 5. -3. 2.))
+                          (define v (vec -3. 4. 5.))
+                          (check-tuple= (mat-t* t v) v)))
+   (test-suite "scaling"
+               (test-case "A scaling matrix applied to a point"
+                          (define t (scale 2. 3. 4.))
+                          (define p (pt -4. 6. 8.))
+                          (check-tuple= (mat-t* t p) (pt -8. 18. 32.)))
+               (test-case "A scaling matrix applied to a vector"
+                          (define t (scale 2. 3. 4.))
+                          (define v (vec -4. 6. 8.))
+                          (check-tuple= (mat-t* t v) (vec -8. 18. 32.)))
+               (test-case "Multiplying by the inverse of a scaling matrix"
+                          (define t (scale 2. 3. 4.))
+                          (define inv (inverse t))
+                          (define v (vec -4. 6. 8.))
+                          (check-tuple= (mat-t* inv v) (vec -2. 2. 2.)))
+               (test-case "Reflection is scaling by a negative value"
+                          (define t (scale -1. 1. 1.))
+                          (define p (pt 2. 3. 4.))
+                          (check-tuple= (mat-t* t p) (pt -2. 3. 4.))))
+   (test-suite
+    "rotation"
+    (test-case "Rotating a point around the x axis"
+               (define p (pt 0. 1. 0.))
+               (define half-quarter (rotate 'x (/ pi 4)))
+               (define full-quarter (rotate 'x (/ pi 2)))
+               (check-tuple= (mat-t* half-quarter p)
+                             (pt 0. (cast (/ (sqrt 2) 2) Float) (cast (/ (sqrt 2) 2) Float)))
+               (check-tuple= (mat-t* full-quarter p) (pt 0. 0. 1.)))
+    (test-case "The inverse of an x-rotation rotates in the opposite direction"
+               (define p (pt 0. 1. 0.))
+               (define half-quarter (rotate 'x (/ pi 4)))
+               (define inv (inverse half-quarter))
+               (check-tuple= (mat-t* inv p)
+                             (pt 0. (cast (/ (sqrt 2) 2) Float) (cast (- (/ (sqrt 2) 2)) Float))))
+    (test-case "Rotating a point around the y axis"
+               (define p (pt 0. 0. 1.))
+               (define half-quarter (rotate 'y (/ pi 4)))
+               (define full-quarter (rotate 'y (/ pi 2)))
+               (check-tuple= (mat-t* half-quarter p)
+                             (pt (cast (/ (sqrt 2) 2) Float) 0. (cast (/ (sqrt 2) 2) Float)))
+               (check-tuple= (mat-t* full-quarter p) (pt 1. 0. 0.)))
+    (test-case "Rotating a point around the z axis"
+               (define p (pt 0. 1. 0.))
+               (define half-quarter (rotate 'z (/ pi 4)))
+               (define full-quarter (rotate 'z (/ pi 2)))
+               (check-tuple= (mat-t* half-quarter p)
+                             (pt (cast (- (/ (sqrt 2) 2)) Float) (cast (/ (sqrt 2) 2) Float) 0.))
+               (check-tuple= (mat-t* full-quarter p) (pt -1. 0. 0.))))))
 
 (run-tests transform-test)
--- transform.rkt
+++ transform.rkt
@@ -14,6 +14,7 @@
                     [(and (= row 1) (= col 3)) y]
                     [(and (= row 2) (= col 3)) z]
                     [else 0.]))))
+
 (: scale (-> Float Float Float Matrix))
 (define (scale x y z)
   (build-matrix 4
@@ -25,3 +26,43 @@
                     [(and (= row 2) (= col 2)) z]
                     [(and (= row 3) (= col 3)) 1.]
                     [else 0.]))))
+
+(: rotate (-> (U 'x 'y 'z) Float Matrix))
+(define (rotate axis rad)
+  (cond
+    [(eq? axis 'x)
+     (build-matrix 4
+                   4
+                   (lambda ([row : Exact-Nonnegative-Integer] [col : Exact-Nonnegative-Integer])
+                     (cond
+                       [(and (= row 0) (= col 0)) 1.]
+                       [(and (= row 1) (= col 1)) (cos rad)]
+                       [(and (= row 1) (= col 2)) (- (sin rad))]
+                       [(and (= row 2) (= col 1)) (sin rad)]
+                       [(and (= row 2) (= col 2)) (cos rad)]
+                       [(and (= row 3) (= col 3)) 1.]
+                       [else 0.])))]
+    [(eq? axis 'y)
+     (build-matrix 4
+                   4
+                   (lambda ([row : Exact-Nonnegative-Integer] [col : Exact-Nonnegative-Integer])
+                     (cond
+                       [(and (= row 0) (= col 0)) (cos rad)]
+                       [(and (= row 0) (= col 2)) (sin rad)]
+                       [(and (= row 1) (= col 1)) 1.]
+                       [(and (= row 2) (= col 0)) (- (sin rad))]
+                       [(and (= row 2) (= col 2)) (cos rad)]
+                       [(and (= row 3) (= col 3)) 1.]
+                       [else 0.])))]
+    [(eq? axis 'z)
+     (build-matrix 4
+                   4
+                   (lambda ([row : Exact-Nonnegative-Integer] [col : Exact-Nonnegative-Integer])
+                     (cond
+                       [(and (= row 0) (= col 0)) (cos rad)]
+                       [(and (= row 0) (= col 1)) (- (sin rad))]
+                       [(and (= row 1) (= col 0)) (sin rad)]
+                       [(and (= row 1) (= col 1)) (cos rad)]
+                       [(and (= row 2) (= col 2)) 1.]
+                       [(and (= row 3) (= col 3)) 1.]
+                       [else 0.])))]))
