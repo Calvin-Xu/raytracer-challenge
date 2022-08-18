e1d24180516ee414b91d257ce8a391a9096593c7
Calvin Xu <calvinxu806@gmail.com>
2022-08-18 15:05:29 -0700

    Bug fix to canvas; incorrect x and y parameter positions

--- canvas.rkt
+++ canvas.rkt
@@ -21,7 +21,7 @@
           (vector->immutable-vector
            (build-vector (ann (* width height) Integer)
                          (lambda ([n : Exact-Nonnegative-Integer])
-                           (f (exact-floor (/ n width)) (remainder n width)))))))
+                           (f (remainder n width) (exact-floor (/ n width))))))))
 
 (: pixel-at (-> Canvas Exact-Nonnegative-Integer Exact-Nonnegative-Integer Color))
 (define (pixel-at canvas x y)
