86f07938dccfdbc9348675164293b637cdd1668b
Calvin Xu <calvinxu806@gmail.com>
2022-09-01 13:46:47 -0700

    Moved all source tangle block to end of document & reflect into tuples

--- light.rkt
+++ light.rkt
@@ -5,7 +5,3 @@
 
 (struct light ([id : String] [position : Point] [intensity : Color]) #:prefab #:type-name Light)
 (struct point-light light () #:prefab #:type-name PointLight)
-
-(: reflect (-> Vector Vector Vector))
-(define (reflect in normal)
-  (assert (tuple- in (tuple* normal (* 2 (dot* in normal)))) vect?))
