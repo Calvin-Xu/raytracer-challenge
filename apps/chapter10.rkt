147aa4c5fbb17743d7b2489689b9a968fff5dcdc
Calvin Xu <calvinxu806@gmail.com>
2022-08-31 18:46:09 -0700

    Chapter 10 Patterns complete

---
+++ apps/chapter10.rkt
@@ -0,0 +1,66 @@
+#lang typed/racket
+(require "../tuples.rkt")
+(require "../color.rkt")
+(require "../canvas.rkt")
+(require "../transform.rkt")
+(require "../shapes.rkt")
+(require "../material.rkt")
+(require "../patterns.rkt")
+(require "../light.rkt")
+(require "../shading.rkt")
+(require "../world.rkt")
+(require "../camera.rkt")
+
+
+(define common-material
+  (make-material #:color (color 1. 0.9 0.9)
+                 #:specular 0.
+                 #:pattern (pattern 'checker
+                             (list white black)
+                             #:transformation (rotate 'y (/ pi 4)))))
+
+(define floor (plane "floor" #:material common-material))
+
+(define middle
+  (sphere "middle-sphere"
+          #:transformation (translate -0.5 1. 0.5)
+          #:material (make-material #:color (color 0.1 1. 0.5)
+                                    #:diffuse 0.7
+                                    #:specular 0.3
+                                    #:pattern (pattern 'gradient
+                                                (list (color 0.1 0. 0.6) (color 1. 0. 0.1))
+                                                #:transformation (scale 0.5 0.5 0.5)))))
+
+(define right
+  (sphere "right-sphere"
+          #:transformation (transformation (scale 0.5 0.5 0.5) (translate 1.5 0.5 -0.5))
+          #:material (make-material #:color (color 0.5 1. 0.1)
+                                    #:diffuse 0.7
+                                    #:specular 0.3
+                                    #:pattern (pattern 'stripe
+                                                (list (color 0.4 0.8 0.1) (color 0. 0.4 0.8))
+                                                #:transformation (scale 0.2 0.2 0.2)))))
+
+(define left
+  (sphere "left-sphere"
+          #:transformation (transformation (scale 0.33 0.33 0.33) (translate -1.5 0.33 -0.75))
+          #:material (make-material #:color (color 1. 0.8 0.1)
+                                    #:diffuse 0.7
+                                    #:specular 0.3
+                                    #:pattern (pattern 'ring
+                                                (list (color 1. 0.6 0.2) (color 0.4 0.9 0.))
+                                                #:transformation (scale 0.2 0.2 0.2)))))
+
+(define world
+  (make-world (list floor middle right left)
+              (list (point-light "white-above-left" (pt -10. 10. -10.) (color 0.8 0.8 0.8))
+                    (point-light "white-above-right" (pt 5. 10. -10.) (color 0.8 0.8 0.8)))))
+
+(define camera
+  (make-camera #:hsize 1080
+               #:vsize 720
+               #:fov (/ pi 3)
+               #:transform (view-transformation (pt 0. 1.5 -5.) (pt 0. 1. 0.) (vec 0. 1. 0.))
+               #:aparture-size 0.001))
+
+(save-canvas (render world camera 4) "test.ppm")
